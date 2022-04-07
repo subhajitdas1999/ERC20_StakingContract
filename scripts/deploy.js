e60919d1084e02c11d8fe4aa2cb8d6bcff19423a
Subhajit Das <sd651846@gmail.com>
2022-04-07 19:16:04 +0530

    added scripts for deployment

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Staking = await ethers.getContractFactory("ERC20StakingContract");
+    const Token = await ethers.getContractFactory("MyERC20Token");
+    const initialTokenSupply = 1000000;
+    const token = await Token.deploy("My Token", "MT", initialTokenSupply);
+    const staking = await Staking.deploy(token.address);
+
+    //send all the tokens to the staking contract. so that the staking contract have have balance on its own
+    const deployerTokenBalance = await token.balanceOf(deployer.address);
+    await token.transfer(staking.address, deployerTokenBalance);
+  
+    console.log("Staking address:", staking.address);
+    console.log("ERC20Token address:", token.address);
+
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
