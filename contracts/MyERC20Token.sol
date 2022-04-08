abcb82de5a06f40e7006798298411491627cee94
Subhajit Das <sd651846@gmail.com>
2022-04-08 17:32:54 +0530

    added some comments

--- contracts/MyERC20Token.sol
+++ contracts/MyERC20Token.sol
@@ -7,14 +7,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract MyERC20Token is ERC20,Ownable{
+
     constructor(string memory _name,string memory _symbol,uint _initialBitsSupply) ERC20(_name,_symbol){
         _mint(owner(),_initialBitsSupply);
     }
-
+    //mint tokensBits
     function mint(uint _tokenSupply) public onlyOwner{
         _mint(owner(),_tokenSupply);
     }
-
+    //burn Token Bits
     function burn(uint _tokenBitsAmount) public onlyOwner{
         _burn(owner(),_tokenBitsAmount);
     }
