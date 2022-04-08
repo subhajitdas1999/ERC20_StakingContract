fbfcb777e8f71840106a4701f287ca83eefd21bf
Subhajit Das <sd651846@gmail.com>
2022-04-08 17:17:17 +0530

    made changes in contract, removed all decimal calculations, and change the way of storing staking collections

--- contracts/ERC20StakingContract.sol
+++ contracts/ERC20StakingContract.sol
@@ -2,120 +2,110 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract ERC20StakingContract {
-
-    //count all the staking
-    uint private stakeId;
 
     //ERC20 contract
     IERC20 tokenContract;
 
-    //token contract decimal
-    uint private decimals=18;
-
     //staking details
     struct Staking{
-        uint stakeId;
         address stakingOwner;
         uint tokenBitsAmount;
         uint timeOfStaking;
     }
 
-    //mapp all the stakings. stakeId => Staking.
-    mapping(uint => Staking) public stakings;
-
+    //mapp all the stakings . address => [all the stakings]
+    mapping(address => Staking[]) public stakings;
     //events for staking
-    event staked(address indexed stakingOwner,uint tokenAmount,uint stakeId);
+    event staked(address indexed stakingOwner,uint tokenAmountBits,uint stakeIndex);
     //events for stake withdraw
-    event withdraw(uint withdrawTokenBits,uint remainingTokenBits,uint stakeId);
+    event withdraw(uint withdrawTokenBits,uint remainingTokenBits,uint stakeIndex);
 
     constructor(
         IERC20 _tokenContract
     ){
-        tokenContract = _tokenContract;
-        
+        tokenContract = _tokenContract;       
     }
 
-    //stake contracts
-    function stakeTokens(uint _tokenAmount) public {
-        uint tokenBits = tokenBitsAmount(_tokenAmount);
-
-        require(tokenContract.balanceOf(msg.sender)>= tokenBits,"Account have less token");
-        require(_tokenAmount >=100,"minimum staking balance is 100 tokens");
-
-        //increase the stake id
-        stakeId++;
-
+    //stake contracts .approve this contract before staking.
+    function stakeTokens(uint _tokenBitsAmount) public {
+        require(tokenContract.balanceOf(msg.sender)>= _tokenBitsAmount,"Account have less token");
+        require(tokenContract.allowance(msg.sender,address(this))>=_tokenBitsAmount,"Allowance is less for this contract");
+        require(_tokenBitsAmount >= 100,"minimum staking TokenBits is 100");
+        //construct the struct
         Staking memory staking = Staking(
-            stakeId,
             msg.sender,
-            tokenBits,
+            _tokenBitsAmount,
             block.timestamp
         );
+
         //transfer the tokens from staking owner to contract
-       tokenContract.transferFrom(staking.stakingOwner,address(this),tokenBits);
+       tokenContract.transferFrom(staking.stakingOwner,address(this),_tokenBitsAmount);
 
         //add the staking to the collections
-        stakings[stakeId] = staking;
+        stakings[msg.sender].push(staking);
 
-        //emit the staked events
-        emit staked(staking.stakingOwner,_tokenAmount,stakeId);
+        //emit the staked events .latest stake Index should be length of stakes array -1 .
+        emit staked(msg.sender,_tokenBitsAmount,stakings[msg.sender].length - 1);
     }
 
-    //withdraw stake tokens
-    function withdrawStake(uint _stakeId,uint _tokenAmount) public{
-        require(_stakeId > 0 && _stakeId<=stakeId,"staking Id is not exists");
-        uint tokenBits = tokenBitsAmount(_tokenAmount);
-        Staking storage staking = stakings[_stakeId];
-        require(staking.stakingOwner == msg.sender,"Only staking owner is allowed to withdraw");
-        require(staking.tokenBitsAmount >= tokenBits,"Exceeding tokens amount");
-
-        uint updatedTokenBits = staking.tokenBitsAmount - tokenBits ;
-       //calculate the ROI
-        uint ROItokenBits  = calculateROI(_stakeId,tokenBits);
+    //withdraw stake tokens 
+    function withdrawStake(uint _stakeIndex, uint _tokenBitsAmount) public{
+        //check the stake index
+        require(_stakeIndex < stakings[msg.sender].length,"Invalid Index");
+        //get the staking details from collection
+        Staking storage staking = stakings[msg.sender][_stakeIndex];
+        //check the balance
+        require(staking.tokenBitsAmount >= _tokenBitsAmount,"Exceeding tokens amount");
+        //update the tokenBits
+        uint updatedTokenBits = staking.tokenBitsAmount - _tokenBitsAmount ;
+        require(updatedTokenBits ==0|| updatedTokenBits >= 100,"withdraw all or remaining staking balance should be greater than 100");
+        //calculate the ROI
+        uint ROItokenBits  = calculateROI(_stakeIndex,_tokenBitsAmount);
         //total tokenBits to transfer the stakeOwner
-        uint totalTokenBits = tokenBits + ROItokenBits;
+        uint totalTokenBits = _tokenBitsAmount + ROItokenBits;
         //update the staking collection
         staking.tokenBitsAmount = updatedTokenBits;
         //transfer the tokens to the stake owner from this contract address
         tokenContract.transfer(staking.stakingOwner,totalTokenBits);
 
         //emit the withdraw events
-        emit withdraw(tokenBits,staking.tokenBitsAmount,staking.stakeId);
+        emit withdraw(_tokenBitsAmount,staking.tokenBitsAmount,_stakeIndex);
 
     }
 
-    function calculateROI(uint _stakeId,uint _tokenBits) private view returns(uint){
-        Staking memory staking = stakings[_stakeId];
+    //get all the stakings of a user
+    function getStakings(address _stakeOwner) public view returns(Staking[] memory){
+        return stakings[_stakeOwner];
+    }
+
+    //calculate the ROI based on Investment
+    function calculateROI(uint _stakeIndex,uint _tokenBitsAmount) public view returns(uint){
+        Staking memory staking = stakings[msg.sender][_stakeIndex];
         uint stakingPeriod = block.timestamp-staking.timeOfStaking;
         uint returnPersentage;
-        //if staking period is greater than 1 month and less than 6 month ROI is 5%
-        if (stakingPeriod >= 4 weeks && stakingPeriod < 24 weeks){
+
+        //for testing uncomment it
+        // if (stakingPeriod >= 4 seconds){
+        //     returnPersentage = 5;
+        // }
+        //if staking period is greater than 1 month(30days) and less than 6 month (~183 days) ROI is 5%        
+        if (stakingPeriod >= 30 days && stakingPeriod < 183 days){
             returnPersentage = 5;
         }
-        //if staking period is greater than 6 month and less than 1 year ROI is 10%
-        else if(stakingPeriod >= 24 weeks && stakingPeriod < 48 weeks){
+        //if staking period is greater than 6 month(~183 days) and less than 1 year(365 days) ROI is 10%
+        else if(stakingPeriod >= 183 days && stakingPeriod < 365 days){
             returnPersentage = 10;
         }
         //if staking period is greater than 1 year, ROI is 15%
-        else if(stakingPeriod >= 48 weeks) {
+        else if(stakingPeriod >= 365 days) {
             returnPersentage = 15;
         }
 
         //erc20 decimal is present,considering it into the calculation 
-        return (_tokenBits * returnPersentage)/100 ; 
-
+        return (_tokenBitsAmount * returnPersentage)/100 ; 
     }
-
-    
-
-    //returns the with tokenBits
-    function tokenBitsAmount(uint256 _amount) private view returns (uint256) {
-        return _amount * (10**decimals);
-    }
-
-
-
 
 }
--- contracts/MyERC20Token.sol
+++ contracts/MyERC20Token.sol
@@ -7,28 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract MyERC20Token is ERC20,Ownable{
-    constructor(string memory _name,string memory _symbol,uint _initialSupply) ERC20(_name,_symbol){
-        uint _initialSupplyWithBits = tokenBitsAmount(_initialSupply);
-        _mint(owner(),_initialSupplyWithBits);
+    constructor(string memory _name,string memory _symbol,uint _initialBitsSupply) ERC20(_name,_symbol){
+        _mint(owner(),_initialBitsSupply);
     }
 
     function mint(uint _tokenSupply) public onlyOwner{
-        uint _supplyWithBits = tokenBitsAmount(_tokenSupply);
-        _mint(owner(),_supplyWithBits);
+        _mint(owner(),_tokenSupply);
     }
 
-    function burn(uint _tokenAmount) public onlyOwner{
-        uint _tokenBits = tokenBitsAmount(_tokenAmount);
-        _burn(owner(),_tokenBits);
+    function burn(uint _tokenBitsAmount) public onlyOwner{
+        _burn(owner(),_tokenBitsAmount);
     }
 
-    function approveWithoutTokenBits(address _spender,uint _amount) public{
-        uint _tokenBits = tokenBitsAmount(_amount);
-        approve(_spender,_tokenBits);
-    }
-
-    //returns the with tokenBits
-    function tokenBitsAmount(uint256 _amount) private view returns (uint256) {
-        return _amount * (10**decimals());
-    }
+    
 }
